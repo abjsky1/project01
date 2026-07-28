6cb6f333fa8652bf110801258fe22e17ea5063c5
jaeui777 <jaeui777@gmail.com>
2026-07-28 16:46:17 +0900

    update

---
+++ page4.js
@@ -0,0 +1,37 @@
+let formList = [
+    {
+        fcode: 1,
+        fname: "권유린",
+        fnumber: "01012341234",
+        ffamily: 4,
+        fexp: false,                // true: 반려동물 경험 있음, false: 반려동물 경험 없음
+        freason: "고양이를 입양하고 싶어서",
+        acode: true,                // true: 개인정보 동의함, false: 개인정보 동의 안 함
+        pcode: 6,
+        scode: 4
+    },
+
+    {
+        fcode: 2,
+        fname: "황소연",
+        fnumber: "01043214321",
+        ffamily: 2,
+        fexp: true,
+        freason: "강아지를 키우고 싶어서",
+        acode: true,
+        pcode: 2,
+        scode: 2
+    },
+
+    {
+        fcode: 3,
+        fname: "안훈석",
+        fnumber: "01015613251",
+        ffamily: 1,
+        fexp: false,
+        freason: "외롭습니다",
+        acode: true,
+        pcode: 3,
+        scode: 1
+    }
+];
