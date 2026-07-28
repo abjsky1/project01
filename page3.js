3563e7eee74765405ea5d1b62e73761ee82bd4f7
yurin0109 <yurin0109>
2026-07-28 17:36:26 +0900

    33

--- page3.js
+++ page3.js
@@ -1,14 +1,4 @@
 
-const shList = [
-    { shcode: 1, shname: "성결보호소" },
-    { shcode: 2, shname: "유린보호소" },
-    { shcode: 3, shname: "병준보호소" },
-    { shcode: 4, shname: "훈석보호소" },
-    { shcode: 5, shname: "소연보호소" },
-    { shcode: 6, shname: "시흥보호소" },
-    { shcode: 7, shname: "안양보호소" },
-    { shcode: 8, shname: "서울보호소" }
-];
 
 
 
